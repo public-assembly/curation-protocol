8f80a01d05e9e2054d45affd911d4205cf6de532
0xTranqui <thinkandchill11@gmail.com>
2022-10-07 18:19:46 -0500

    first pass of curator.sol comments

--- src/Curator.sol
+++ src/Curator.sol
@@ -10,7 +10,21 @@ import { CuratorSkeletonNFT } from "./CuratorSkeletonNFT.sol";
 import { IMetadataRenderer } from "./interfaces/IMetadataRenderer.sol";
 import { CuratorStorageV1 } from "./CuratorStorageV1.sol";
 
-contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNFT {
+/**
+ * @notice Base contract for curation functioanlity. Inherits ERC721 standard from CuratorSkeletonNFT.sol
+ *      (curation information minted as non-transferable "listingRecords" to curators to allow for easy integration with NFT indexers)
+ * @dev For curation contracts: assumes 1. linear mint order
+ * @author iain@zora.co
+ *
+ */
+
+contract Curator is 
+    ICurator, 
+    UUPS, 
+    Ownable, 
+    CuratorStorageV1, 
+    CuratorSkeletonNFT 
+{
     // Public constants for curation types.
     // Allows for adding new types later easily compared to a enum.
     uint16 public constant CURATION_TYPE_GENERIC = 0;
@@ -24,7 +38,7 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
     /// @notice Reference to factory contract
     ICuratorFactory private immutable curatorFactory;
 
-    /// @notice Modifier that ensures the curator is active and not frozen
+    /// @notice Modifier that ensures curation functionality is active and not frozen
     modifier onlyActive() {
         if (isPaused && msg.sender != owner()) {
             revert CURATION_PAUSED();
@@ -37,7 +51,7 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         _;
     }
 
-    /// @notice Modifier that only allows an admin or curator of a specific entry access
+    /// @notice Modifier that restricts entry access to an admin or curator
     modifier onlyCuratorOrAdmin(uint256 listingId) {
         if (owner() != msg.sender || idToListing[listingId].curator != msg.sender) {
             revert ACCESS_NOT_ALLOWED();
@@ -46,10 +60,24 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         _;
     }
 
+    /// @notice Global constructor – these variables will not change with further proxy deploys
+    /// @param _curatorFactory Curator Factory Address
+
     constructor(address _curatorFactory) payable initializer {
         curatorFactory = ICuratorFactory(_curatorFactory);
     }
 
+
+    ///  @dev Create a new curation contract
+    ///  @param _owner User that owns and can accesss contract admin functionality
+    ///  @param _name Contract name
+    ///  @param _symbol Contract symbol
+    ///  @param _curationPass ERC721 contract whose ownership gates access to curation functionality
+    ///  @param _pause Sets curation active state upon initialization 
+    ///  @param _curationLimit Sets cap for number of listings that can be curated at any time. Doubles as MaxSupply check. 0 = uncapped 
+    ///  @param _renderer Renderer contract to use
+    ///  @param _rendererInitializer Bytes encoded string to pass into renderer. Leave blank if using SVGMetadataRenderer
+    ///  @param _initialListings Array of Listing structs to curate (aka mint) upon initialization
     function initialize(
         address _owner,
         string memory _name,
@@ -61,33 +89,38 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         bytes memory _rendererInitializer,
         Listing[] memory _initialListings
     ) external initializer {
+        // Setup owner role
         __Ownable_init(_owner);
-
+        // Setup contract name + symbol
         contractName = _name;
         contractSymbol = _symbol;
-
+        // Setup curation pass. MUST be set to a valid ERC721 address
         curationPass = IERC721Upgradeable(_curationPass);
-
+        // Setup metadata renderer
         _updateRenderer(IMetadataRenderer(_renderer), _rendererInitializer);
-
+        // Setup initial curation active state
         if (_pause) {
             _setCurationPaused(_pause);
         }
-
+        // Setup intial curation limit
         if (_curationLimit != 0) {
             _updateCurationLimit(_curationLimit);
         }
-
+        // Setup initial listings to curate
         if (_initialListings.length != 0) {
             _addListings(_initialListings, _owner);
         }
     }
 
+    /// @dev Getter for acessing Listing information for a specific tokenId
+    /// @return boolean if address is admin
     function getListing(uint256 index) external view override returns (Listing memory) {
         ownerOf(index);
         return idToListing[index];
     }
 
+    /// @dev Getter for acessing Listing information for all active listings
+    /// @return boolean if address is admin
     function getListings() external view override returns (Listing[] memory activeListings) {
         unchecked {
             activeListings = new Listing[](numAdded - numRemoved);
@@ -106,14 +139,21 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
     }
 
     /**
-        Admin functions
-     */
+     *** ---------------------------------- ***
+     ***                                    ***
+     ***          ADMIN FUNCTIONS           ***
+     ***                                    ***
+     *** ---------------------------------- ***
+     ***/
 
+    /// @dev Allows contract owner to update curation limit
     function updateCurationLimit(uint256 newLimit) external onlyOwner {
         _updateCurationLimit(newLimit);
     }
 
     function _updateCurationLimit(uint256 newLimit) internal {
+
+        // Prevents owner from updating curationLimit below current number of active Listings
         if (curationLimit < newLimit && curationLimit != 0) {
             revert CANNOT_UPDATE_CURATION_LIMIT_DOWN();
         }
@@ -121,7 +161,10 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         emit UpdatedCurationLimit(newLimit);
     }
 
+    /// @dev Allows contract owner to freeze all contract functionality starting from a given Unix timestamp
     function freezeAt(uint256 timestamp) external onlyOwner {
+
+        // Prevents owner from adjusting freezeAt time if contract alrady frozen
         if (frozenAt != 0 && frozenAt < block.timestamp) {
             revert CURATION_FROZEN();
         }
@@ -129,12 +172,14 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         emit ScheduledFreeze(frozenAt);
     }
 
+    /// @dev Allows contract owner to update renderer address and pass in an optional initializer for the new renderer
     function updateRenderer(address _newRenderer, bytes memory _rendererInitializer) external onlyOwner {
         _updateRenderer(IMetadataRenderer(_newRenderer), _rendererInitializer);
     }
 
     function _updateRenderer(IMetadataRenderer _newRenderer, bytes memory _rendererInitializer) internal {
         renderer = _newRenderer;
+
         // If data provided, call initalize to new renderer replacement.
         if (_rendererInitializer.length > 0) {
             renderer.initializeWithData(_rendererInitializer);
@@ -142,13 +187,17 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         emit SetRenderer(address(renderer));
     }
 
+    /// @dev Allows contract owner to update the ERC721 Curation Pass being used to restrict access to curation functionality
     function updateCurationPass(IERC721Upgradeable _curationPass) public onlyOwner {
         curationPass = _curationPass;
 
         emit TokenPassUpdated(msg.sender, address(_curationPass));
     }
 
+    /// @dev Allows contract owner to update the ERC721 Curation Pass being used to restrict access to curation functionality
     function setCurationPaused(bool _setPaused) public onlyOwner {
+        
+        // Prevents owner from updating the curation active state to the current active state
         if (isPaused == _setPaused) {
             revert CANNOT_SET_SAME_PAUSED_STATE();
         }
@@ -162,15 +211,26 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         emit CurationPauseUpdated(msg.sender, isPaused);
     }
 
-    /** 
-        Curator Functions
-    */
+    /**
+     *** ---------------------------------- ***
+     ***                                    ***
+     ***         CURATOR FUNCTIONS          ***
+     ***                                    ***
+     *** ---------------------------------- ***
+     ***/
 
+    /// @dev Allows owner or curator to curate Listings --> which mints a listingRecord token to the msg.sender
     function addListings(Listing[] memory listings) external onlyActive {
+        
+        // Access control for non owners to acess addListings functionality 
         if (msg.sender != owner()) {
+            
+            // ensures that curationPass is a valid ERC721 address
             if (address(curationPass).code.length == 0) {
                 revert PASS_REQUIRED();
             }
+
+            // checks if non-owner msg.sender owns the Curation Pass
             try curationPass.balanceOf(msg.sender) returns (uint256 count) {
                 if (count == 0) {
                     revert PASS_REQUIRED();
@@ -201,7 +261,10 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         }
     }
 
+    /// @dev Allows owner or curator to curate Listings --> which mints listingRecords to the msg.sender
     function updateSortOrders(uint256[] calldata tokenIds, int32[] calldata sortOrders) external onlyActive {
+        
+        // prevents users from submitting invalid inputs
         if (tokenIds.length != sortOrders.length) {
             revert INVALID_INPUT_LENGTH();
         }
@@ -211,18 +274,28 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         emit UpdatedSortOrder(tokenIds, sortOrders, msg.sender);
     }
 
+    // prevents non-owners from updating the SortOrder on a listingRecord they did not curate themselves 
     function _setSortOrder(uint256 listingId, int32 sortOrder) internal onlyCuratorOrAdmin(listingId) {
         idToListing[listingId].sortOrder = sortOrder;
     }
 
     /**
-        NFT Functions
-     */
+     *** ---------------------------------- ***
+     ***                                    ***
+     ***     listingRecord NFT Functions    ***
+     ***                                    ***
+     *** ---------------------------------- ***
+     ***/
 
+    /// @dev allows owner or curators to burn a specfic listingRecord NFT which also removes it from the listings mapping
     function burn(uint256 listingId) public onlyActive {
+
+        // ensures that msg.sender must be contract owner or the curator of the specific listingId 
         _burnTokenWithChecks(listingId);
     }
 
+
+/// @dev allows owner or curators to burn specfic listingRecord NFTs which also removes them from the listings mapping
     function burnBatch(uint256[] calldata listingIds) external onlyActive {
         unchecked {
             for (uint256 i = 0; i < listingIds.length; ++i) {
@@ -262,10 +335,12 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
         return idToListing[id].curator;
     }
 
+    /// @notice Token URI Getter, proxies to metadataRenderer
     function tokenURI(uint256 tokenId) external view override returns (string memory) {
         return renderer.tokenURI(tokenId);
     }
 
+    /// @notice Contract URI Getter, proxies to metadataRenderer
     function contractURI() external view override returns (string memory) {
         return renderer.contractURI();
     }
@@ -285,9 +360,16 @@ contract Curator is ICurator, UUPS, Ownable, CuratorStorageV1, CuratorSkeletonNF
     }
 
     /**
-        Contract admin functions
-     */
+     *** ---------------------------------- ***
+     ***                                    ***
+     ***         UPGRADE FUNCTIONS          ***
+     ***                                    ***
+     *** ---------------------------------- ***
+     ***/
 
+    /// @notice Connects this contract to the factory upgrade gate
+    /// @param _newImpl proposed new upgrade implementation    
+    /// @dev Only can be called by contract owner    
     function _authorizeUpgrade(address _newImpl) internal view override onlyOwner {
         if (!curatorFactory.isValidUpgrade(_getImplementation(), _newImpl)) {
             revert INVALID_UPGRADE(_newImpl);
